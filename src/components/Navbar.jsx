1f2ea68e6d3f8e2c5141242e26df6559e53b98b4
karim <sheriefkarim@gmail.com>
2024-02-02 00:51:07 +0200

    fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,22 @@ import { Navbar as NavbarBS, Nav, Container } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
 
 export default function Navbar() {
-  const buttonRef = useRef();
-  const [isScroll, setIsScroll] = useState(false);
   const { pathname } = useLocation();
+  const buttonRef = useRef();
+  const menuRef = useRef();
+  const [isScroll, setIsScroll] = useState(false);
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       setIsScroll(window.scrollY >= 60);
     });
   }, []);
+
   useEffect(() => {
     if (window.innerWidth <= 768) {
-      buttonRef.current.click();
+      if (menuRef.current.classList.contains("show")) {
+        buttonRef.current.click();
+      }
     }
   }, [pathname]);
 
@@ -26,14 +31,14 @@ export default function Navbar() {
     >
       <Container>
         <NavbarBS.Brand as={NavLink} to="/" className="fs-3 fw-bold">
-          Framework
+          &lt;Framework/&gt;
         </NavbarBS.Brand>
         <NavbarBS.Toggle
           aria-controls="basic-navbar-nav"
           className="shadow-none border-0"
           ref={buttonRef}
         />
-        <NavbarBS.Collapse className="justify-content-end">
+        <NavbarBS.Collapse className="justify-content-end" ref={menuRef}>
           <Nav className="gap-2 mt-3 mt-md-0">
             <Nav.Link as={NavLink} to="/">
               Home
